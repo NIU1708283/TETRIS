a0af2652867edaf5460b8a8010fc232de81cd326
Lenovo <Lenovo@PORTATIL-DE-FEL>
2024-03-31 16:40:17 +0200

    Agregar archivos de proyecto.

---
+++ Joc.cpp
@@ -0,0 +1,24 @@
+#include "Joc.h"
+
+void Joc::inicialitza(const string& nomFitxer)
+{
+}
+
+bool Joc::giraFigura(DireccioGir direccio)
+{
+	return false;
+}
+
+bool Joc::mouFigura(int dirX)
+{
+	return false;
+}
+
+int Joc::baixaFigura()
+{
+	return 0;
+}
+
+void Joc::escriuTauler(const string& nomFitxer)
+{
+}
